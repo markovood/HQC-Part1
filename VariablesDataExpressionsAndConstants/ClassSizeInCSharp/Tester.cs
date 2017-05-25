afb1eab0d3f835f65387a157446be49f7ade6732
Georgi Markov <markov_ood@abv.bg>
2017-05-26 00:50:18 +0300

    finished homework

---
+++ VariablesDataExpressionsAndConstants/ClassSizeInCSharp/Tester.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSizeInCSharp
+{
+    public class Size
+    {
+        private double width, height;
+        public Size(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public static Size GetRotatedSize(
+            Size dimensions, double angle)
+        {
+            double width = Math.Abs(Math.Cos(angle)) * dimensions.Width +
+                            Math.Abs(Math.Sin(angle)) * dimensions.Height;
+            double height = Math.Abs(Math.Sin(angle)) * dimensions.Width +
+                            Math.Abs(Math.Cos(angle)) * dimensions.Height;
+
+            var rotatedSize = new Size(width, height);
+            return rotatedSize;
+        }
+    }
+
+    public class Tester
+    {
+        public static void Main()
+        {
+        }
+    }
+}
---
+++ VariablesDataExpressionsAndConstants/MethodPrintStatisticsInCSharp/Tester.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPrintStatisticsInCSharp
+{
+    public class Tester
+    {
+        public void PrintStatistics(double[] arr, int count)
+        {
+            double max = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            PrintMax(max);
+
+            double min = max;
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            PrintMin(min);
+
+            double sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sum += arr[i];
+            }
+
+            PrintAvg(sum / count);
+        }
+
+        private void PrintMax(double max)
+        {
+            Console.WriteLine(max);
+        }
+
+        private void PrintMin(double min)
+        {
+            Console.WriteLine(min);
+        }
+
+        private void PrintAvg(double average)
+        {
+            Console.WriteLine(average);
+        }
+
+        public static void Main()
+        {
+            var t = new Tester();
+            t.PrintStatistics(new double[] { 4, 5, 6, 7, 8, 9 }, 6);
+        }
+    }
+}
